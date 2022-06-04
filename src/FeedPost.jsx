73a75c0549b24f54f534d76fb45f356cf8ccc3b5
https://github.com/Samandar0021305 <samandar021325@gmail.com>
2022-06-04 15:47:35 +0500

    commit

---
+++ src/FeedPost.jsx
@@ -0,0 +1,53 @@
+import img1 from "./pictures/1-img.webp";
+import img2 from "./pictures/2-img.jpg";
+import img3 from "./pictures/3-img.jfif";
+import img4 from "./pictures/4-img.jfif";
+import img5 from "./pictures/5-img.jpg";
+import img6 from "./pictures/6-img.jpg";
+import img7 from "./pictures/7-img.jpg";
+import img8 from "./pictures/8-img.png";
+
+const response = [
+    {
+        id:12,
+        post:"quidem molestiae enim",
+        img:img1
+    },
+    {
+        id:9,
+        post:"sunt qui excepturi placeat culpa",
+        img:img2
+    },
+    {
+        id:20,
+        post:"non esse culpa molestiae omnis sed optio",
+        img:img3
+    },
+    {
+        id:87,
+        post:"natus impedit quibusdam illo est",
+        img:img4
+    },
+    {
+        id:52,
+        post:"quibusdam autem aliquid et et quia",
+        img:img5
+    },
+    {
+        id:61,
+        post:"saepe unde necessitatibus rem",
+        img:img6
+    },
+    {
+        id:72,
+        post:"quam nostrum impedit mollitia quod et dolor",
+        img:img7
+    },
+    {
+        id:18,
+        post:"sunt qui excepturi placeat culpa",
+        img:img8
+    },
+]
+
+export default response;
--- src/components/post/Post.jsx
+++ src/components/post/Post.jsx
@@ -4,40 +4,50 @@ import {MoreVert,ThumbUp,Favorite} from "@mui/icons-material";
 import myLogo from "../../assets/pexels-photo-220453.webp"
 import logo from "../../assets/post.webp";
 import { useState } from "react";
+
+import response from "../../FeedPost";
+
+
 export default function Post() {
-    const [x,setX] = useState(23)
- const handl = ()=>{
-     setX(x+1)
- }
-  return (
+    const [x,setX] = useState(Math.floor(Math.random()*100))
+    
+    const handl = ()=>{
+        setX(x + 1)
+    }
+    
+      return (
     <div className="post">
-        <div className="postWrapper">
+       {response.map((res)=>{
+           return(
+            <div className="postWrapper">
             <div className="postTop">
                 <div className="postTopLeft">
                     <img className="postProfileImg" src={myLogo} alt="" />
                     <span className="postUserName">Safak Kochugali</span>
-                    <span className="postDate">5 min ago</span>
+                    <span className="postDate">{res.id} min ago</span>
                 </div>
                 <div className="postTopRight">
                     <MoreVert />
                 </div>
             </div>
             <div className="postCenter">
-                <span className="postText">Hey its mey post :)</span>
+                <span className="postText">{res.post}</span>
 
-                <img className="postImg" src={logo} alt="logo" />
+                <img className="postImg" src={res.img} alt="logo" />
             </div>
             <div className="postBottom">
                 <div className="postBottomLeft">
-                    <ThumbUp className="likeIcon" onClick={handl}  htmlColor="blue" />
-                    <Favorite className="likeIcon" onClick={handl} htmlColor="red" />
-                    <span className="postLikeCounter">{x} people like it </span>
+                    <ThumbUp className="likeIcon" key={res.id} onClick={handl}  htmlColor="blue" />
+                    <Favorite className="likeIcon" key={res.id}  onClick={handl} htmlColor="red" />
+                    <span className="postLikeCounter" key={res.id}>{x} people like it </span>
                 </div>
                 <div className="postBottomRigth">
-                    <span className="postCommentText">9 comment</span>
+                    <span className="postCommentText">{res.id} comment</span>
                 </div>
             </div>
         </div>
+           )
+       })}
     </div>
   )
 }
